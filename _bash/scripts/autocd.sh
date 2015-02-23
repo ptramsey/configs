42f2970cedf87b2a5d7223cd3aabd6a99cf0e4aa
Patrick Taylor Ramsey <ptr@fivestars.com>
2015-02-23 12:37:08 -0800

    Add comments and license and copyright notice

--- _bash/scripts/autocd.sh
+++ _bash/scripts/autocd.sh
@@ -1,7 +1,46 @@
+# Copyright (c) Patrick Taylor Ramsey, All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+
+# Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer. Redistributions in binary
+# form must reproduce the above copyright notice, this list of conditions and
+# the following disclaimer in the documentation and/or other materials provided
+# with the distribution. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+# CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
+# NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# Implements a forwards and reverse directory history on top of bash, along 
+# with a version of autocd that respects it.  Commands defined here:
+#
+# up [N] - jump up N levels (1 if omitted) in the directory hierarchy
+# back|b [n] - go back N directories (1 if omitted)
+# forward|f [n] - go forward N directories (1 if omitted)
+# cd [DIR|~|-] - behaves like cd, but stores a directory history
+# [DIR|~] - Automatically cd to DIR (or ~) if DIR is not already the name
+#           of a command
+#
+# Autocd works using bash's DEBUG faux-signal trap (which executes before
+# every command is run) to detect if the user is attempting to autocd, and,
+# if so, to block stderr so the failing command doesn't echo anything.  If
+# we are doing an automatic cd, then the actual chdir is performed (and 
+# stderr restored) from $PROMT_COMMAND, which executes before the next
+# prompt is shown.
+
 pushd() {
     command pushd "$@" && unset _forwards
 }
 
+# Jump up N levels in the directory hierarchy.
 up() {
     test -z "$1" && set 1
     if test "$1" -gt 0; then
@@ -11,6 +50,7 @@ up() {
     fi
 }
 
+# Jump back N directories.
 back() {
     test -z "$1" && set 1
     if test "$1" -gt 0; then
@@ -19,6 +59,7 @@ back() {
     fi
 }
 
+# Jump forwards N directories.
 forwards() {
     test -z "$1" && set 1
     if test "$1" -gt 0; then
@@ -46,25 +87,31 @@ alias b=back
 alias u=up
 alias f=forwards
 
+# Detect if the user is attempting to auto-cd
 prep_autocd() {
     test "$1" = '~' && set "${HOME}"
 
+    # If the command doesn't exist, but a directory by that name does,
     if ! command -v "$1" >/dev/null && test -d "$1" && test -z "$_autocd_dir"; then
         echo -e "\033[01;32mautocd:\033[0m $(readlink -f "$1")";
 
+        # Store the fact that we're in an autocd,
         _autocd_dir="$1"
 
-        # Hide output from unsuccessful command
+        # and hide the 'command not found' message when bash attempts to execute
+        # the directory name.
         exec {_autocd_stderr}>&2;
         exec 2>/dev/null;
     fi
 }
 
 autocd() {
+    # If we are automatically changing directory,
     if test -n "$_autocd_dir"; then
-        # Unblock stderr
+        # unblock stderr,
         test -n "$_autocd_stderr" && exec 2>&$_autocd_stderr-;
         
+        # and perform the actual cd.
         cd "$_autocd_dir";
 
         unset _autocd_stderr;
@@ -72,6 +119,9 @@ autocd() {
     fi
 }
 
+# $BASH_COMMAND is the actual literal command the user entered (including backslashes
+# and quotes), so there's no way to know what the actual command name is without
+# handing it off to another instance of bash to parse any escaped or quoted spaces.
 parse_argv() {
     argv="$1"
     bash -c 'split() { while test -n "$1"; do echo "$1"; shift; done; }; '"split $argv"
